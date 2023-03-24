cf8244c2d6b5c1fd5afed44b300c2b33909f83e0
Tres Henry <tres@treshenry.net>
2023-03-24 15:02:35 +0100

    Starting point for TailwindCSS, elixir, emmet for Phoenix 1.7

--- init.lua
+++ init.lua
@@ -16,3 +16,77 @@ if astronvim.default_colorscheme then
 end
 
 require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+
+local lspconfig = require("lspconfig")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+lspconfig.tailwindcss.setup({
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js',
+  'postcss.config.ts', 'package.json', 'node_modules', '.git', 'mix.exs'),
+  filetypes = { "html", "elixir", "eelixir", "heex" },
+  init_options = {
+    userLanguages = {
+      elixir = "html-eex",
+      eelixir = "html-eex",
+      heex = "html-eex",
+    },
+  },
+  settings = {
+    tailwindCSS = {
+      experimental = {
+        classRegex = {
+          'class[:]\\s*"([^"]*)"',
+        },
+      },
+    },
+  },
+})
+
+lspconfig.emmet_ls.setup({
+  capabilities = capabilities,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "elixir", "eelixir", "heex" }
+})
+
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+cmp.setup({
+  window = {
+    completion = {
+      col_offset = -3 -- align the abbr and word on cursor (due to fields order below)
+    }
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = lspkind.cmp_format({
+      mode = 'symbol_text', -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+      maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      menu = ({             -- showing type in menu
+        nvim_lsp = "[LSP]",
+        path = "[Path]",
+        buffer = "[Buffer]",
+        luasnip = "[LuaSnip]",
+      }),
+      before = function(entry, vim_item) -- for tailwind css autocomplete
+        if vim_item.kind == 'Color' and entry.completion_item.documentation then
+          local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+          if r then
+            local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
+            local group = 'Tw_' .. color
+            if vim.fn.hlID(group) < 1 then
+              vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
+            end
+            vim_item.kind = "■" -- or "⬤" or anything
+            vim_item.kind_hl_group = group
+            return vim_item
+          end
+        end
+        -- vim_item.kind = icons[vim_item.kind] and (icons[vim_item.kind] .. vim_item.kind) or vim_item.kind
+        -- or just show the icon
+        vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+        return vim_item
+      end
+    })
+  }
+})
